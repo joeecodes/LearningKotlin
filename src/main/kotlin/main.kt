6eed9c1413f7f5f167d64378d999ef26d4db203d
notHypews <joehosten@outlook.com>
2021-10-31 15:52:15 +0000

    Update variables

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,10 +1,8 @@
-
-
 // Variables outside of the main function are top level variables
 val name: String = "Joe" // A val cannot be reassigned later on
 var currentLocation: String? = "United Kingdom" // A var can be reassigned later on
 
 fun main() {
     println("Hello Kotlin")
-    println(name + " from " + currentLocation)
+    println("$name from $currentLocation") // You can use "$" inside a string to reference variables
 }
